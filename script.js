373fa33d728ef3246cc8761a1ec9cc6da509ddc3
Justin Zaager <hextechprotect@gmail.com>
2022-06-28 21:13:36 -0400

    Initialize index.html and link script.js
    Add pseudocode to script

---
+++ script.js
@@ -0,0 +1,67 @@
+/* Rock, Paper, Scissors!
+==============================
+* A game to be played entirely in the console (for now)
+* Choose Rock, Paper, or Scissors and see if you can 
+* beat the computer! Good luck!
+*/
+
+####################################
+
+Pseduocode for Rock, Paper, Scissors
+
+####################################
+
+INPUT: Prompt Player for Rock, Paper, or Scissors and store value in variable
+!!Allow input to be case insensitive!!
+
+CPU selects R,P,S
+  Generate a random number 1, 2, or 3 to represent R,P,S
+    Math.floor((Math.random() *  3) + 1))
+    If number is 1
+      choice = rock
+    if number is 2
+      choice = paper
+    if number is 3
+      choice = scissors
+    return choice;
+
+Compare Player choice and CPU choice 
+switch (true) 
+{
+  case (playerChoice == rock && compChoice == paper): 
+    Message Computer wins!
+    break;
+
+  case (playerChoice == rock && compChoice == scissors):
+    Message Player wins!
+    break;
+
+  case (playerChoice == paper && compChoice == rock):
+    Message Player wins!
+    break;
+
+  case (playerChoice == paper && compChoice == scissors):
+    Message Computer wins!
+    break;
+
+  case (playerChoice == scissors && compChoice == rock):
+    Message Computer wins!
+    break;
+
+  case (playerChoice == scissors && compChoice == paper):
+    Message Player wins!
+    break;
+
+  default:
+    Message It's a tie!
+}
+
+Return Message;
+
+Run the Round function 5 times
+  (for let i = 0; i < 5; i++)
+  
+
+
+OUTPUT: Player RPS choice + CPU RPS choice
+        (round) Player win, CPU win, or Tie

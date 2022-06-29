355ad2824ebd67a9e69964d0ee1d32d19a6dec8f
Justin Zaager <hextechprotect@gmail.com>
2022-06-28 21:37:08 -0400

    Add playerChoice function to allow user input

--- script.js
+++ script.js
@@ -5,15 +5,22 @@
 * beat the computer! Good luck!
 */
 
-####################################
+//INPUT: Prompt Player for Rock, Paper, or Scissors and store value in variable
+//!!Allow input to be case insensitive!!
 
-Pseduocode for Rock, Paper, Scissors
+let weapon;
 
-####################################
+// Prompt player for choice of weapon and store it in weapon variable
+function playerChoice() 
+{
+  let input = prompt("Rock, Paper, or Scissors?");
+  // Force the input to uppercase
+  weapon = input.toUpperCase();
+  return weapon;
+}
+  
 
-INPUT: Prompt Player for Rock, Paper, or Scissors and store value in variable
-!!Allow input to be case insensitive!!
-
+/*
 CPU selects R,P,S
   Generate a random number 1, 2, or 3 to represent R,P,S
     Math.floor((Math.random() *  3) + 1))
@@ -24,7 +31,7 @@ CPU selects R,P,S
     if number is 3
       choice = scissors
     return choice;
-
+/*
 Compare Player choice and CPU choice 
 switch (true) 
 {
@@ -65,3 +72,5 @@ Run the Round function 5 times
 
 OUTPUT: Player RPS choice + CPU RPS choice
         (round) Player win, CPU win, or Tie
+
+        */
